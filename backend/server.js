befbbdbea8e51543aa46f7fcfdbcadcb8ded8437
tayyabameraj <tayyabameraj9@gmail.com>
2022-08-09 08:57:28 -0700

    Final Update

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv")
 const productRoutes = require("./routes/productRoutes")
 const userRoutes = require("./routes/userRoutes")
 const orderRoutes = require("./routes/orderRoutes")
+const path = require('path')
 
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 // Env Config
@@ -19,6 +20,14 @@ app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
 app.use(notFound)
 app.use(errorHandler)
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'))
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+})
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(
