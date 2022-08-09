4df98fd23247f6f8993d4c811c03664cbf01dcb1
tayyabameraj <tayyabameraj9@gmail.com>
2022-08-09 14:06:16 -0700

    Changes in Server File

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,7 @@ app.use(express.json())
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
-app.use(notFound)
-app.use(errorHandler)
+
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'))
@@ -29,6 +28,10 @@ if(process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 })
 }
+
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
